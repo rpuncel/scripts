17245634a0047346da535a15e4312b58919ba427
Rob Puncel <rpuncel@gmail.com>
2024-02-01 22:39:38 -0800

    get rid of heading

--- icalfullcalendar.sh
+++ icalfullcalendar.sh
@@ -2,8 +2,6 @@
 
 cal=$(icalbuddy --dateFormat %Y-%m-%d --timeFormat %H:%M -nrd --noCalendarNames  -ss '' --excludeEventProps location,notes,attendees -ps ', | ,' -b ''  eventsToday+3)
 
-echo "| Event | Time |"
-echo "|-------|------|"
 while IFS='\n' read -r line; do
 	echo "$line"' |'
 done <<< "$cal"
